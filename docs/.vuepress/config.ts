8e6d3c361c894954123f371aebd7a5b39b359ac0
chen.zhang <zhangchen64d@gmail.com>
2021-10-25 19:07:39 +0900

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,11 +3,46 @@ import type { DefaultThemeOptions } from "vuepress";
 
 export default defineUserConfig<DefaultThemeOptions>({
   lang: "en-US",
-  title: "Hello VuePress",
-  description: "Just playing around",
+  title: "chen.zhang's blog",
 
   themeConfig: {
-    logo: "https://vuejs.org/images/logo.png",
+    logo: "",
+    navbar: [
+      {
+        text: "俺の日本語",
+        link: "/nihongo/",
+      },
+    ],
+    sidebar: {
+      "/nihongo/": [
+        {
+          text: "JLPT聴解",
+          children: [
+            {
+              text: "n1.201707",
+              link: "/nihongo/jlpt/n1.201707.md",
+            },
+            {
+              text: "n1.201712",
+              link: "/nihongo/jlpt/n1.201712.md",
+            },
+          ],
+        },
+        {
+          text: "曲の練習",
+          children: [
+            {
+              text: "アイロニ",
+              link: "/nihongo/kyoku/アイロニ.md",
+            },
+            {
+              text: "夜に駆ける",
+              link: "/nihongo/kyoku/夜に駆ける.md",
+            },
+          ],
+        },
+      ],
+    },
   },
 
   base: "/vuepress-blog/",
