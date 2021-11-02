602c8275978160c9f0e141bfbd9ef85c091925fd
chen.zhang <zhangchen64d@gmail.com>
2021-11-02 23:37:57 +0900

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,6 +4,7 @@ import type { DefaultThemeOptions } from "vuepress";
 export default defineUserConfig<DefaultThemeOptions>({
   lang: "en-US",
   title: "強ければ強いほど責任を取りなさい",
+  base: "/vuepress-blog/",
 
   themeConfig: {
     logo: "",
@@ -17,37 +18,21 @@ export default defineUserConfig<DefaultThemeOptions>({
       "/nihongo/": [
         {
           text: "JLPT聴解",
-          children: [
-            {
-              text: "N1 2017 07",
-              link: "/nihongo/jlpt/n1.201707.md",
-            },
-            // {
-            //   text: "n1.201712",
-            //   link: "/nihongo/jlpt/n1.201712.md",
-            // },
-          ],
+          children: ["/nihongo/jlpt/n1.201707.md"],
+        },
+        {
+          text: "文法",
+          children: ["/nihongo/try/n1.md"],
         },
         {
           text: "曲の練習",
           children: [
-            {
-              text: "アイロニ",
-              link: "/nihongo/kyoku/アイロニ.md",
-            },
-            {
-              text: "夜に駆ける",
-              link: "/nihongo/kyoku/夜に駆ける.md",
-            },
-            {
-              text: "ドライフラワー",
-              link: "/nihongo/kyoku/ドライフラワー.md",
-            },
+            "/nihongo/kyoku/アイロニ.md",
+            "/nihongo/kyoku/夜に駆ける.md",
+            "/nihongo/kyoku/ドライフラワー.md",
           ],
         },
       ],
     },
   },
-
-  base: "/vuepress-blog/",
 });
